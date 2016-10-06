63997d9519de81bc755b4d26dbead014ceae0d6b
fdantas78 <development@dantas.me>
2016-10-06 15:52:44 +0000

    Finished Create method

--- config/routes.rb
+++ config/routes.rb
@@ -6,16 +6,19 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   
   root 'pages#home'
-  get 'about', to: 'pages#about'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  
+  get 'about', to: 'pages#about'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  
+  resources :articles
 
   # Example resource route with options:
   #   resources :products do
